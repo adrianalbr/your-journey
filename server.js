b8b6951f37d277de75cac4077aefc01ffb8aaaac
Adriana Bonilla-Ramirez <adrianalbr12@gmail.com>
2021-03-14 23:13:01 -0400

    basic server set up

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const contentRoutes = require("./routes/contentRoutes");
+const auth = require ("./middleware/auth");
+
+// const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+require("./routes/userRoutes.js")(app);
+app.use("/api/content",auth , contentRoutes);
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/your-journey",
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
+
+const connection = mongoose.connection;
+
+connection.on("connected", () => {
+  console.log("Mongoose successfully connected!");
+});
+
+connection.on("error", (err) => {
+  console.log("Mongoose connection error: ", err);
+});
+
+app.get("/api/config", (req, res) => {
+  res.json({
+    success: true,
+  });
+});
+
+// Start the API server
+app.listen(PORT, function () {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
+
